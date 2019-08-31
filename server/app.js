25dd434be293d2a12dd2bf10bf11acb9f1ecac7d
Nuttapon <bankjumpmon@hotmail.com>
2019-08-31 14:07:18 +0700

    add server

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const path = require('path');
+
+const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
+
+app.get('/', (req, res) => { 
+  res.send('Hello World!') 
+})
+
+app.post('/login', (req, res) => { 
+  res.json({
+    success: true,
+    token: '12345',
+    message: 'Login success'
+  })
+})
+
+module.exports = app;
